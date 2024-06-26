b282243f1631ff667dfaa1d87402e4d44c84c1df
William Spada <williamrspada02@gmail.com>
2024-06-26 15:24:50 -0300

    refactor: reuse function to create person instance object to test and switch to bun test runner instead jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  rootDir: ".",
-  moduleNameMapper: {
-    "@/(.*)": "<rootDir>/src/$1"
-  }
-};
--- src/__tests__/people.spec.ts
+++ src/__tests__/people.spec.ts
@@ -1,5 +1,6 @@
 import { PeopleService } from '@/services/people';
 import { PeopleRepository } from '@/repositories/people';
+import { Person } from '@/models/person';
 
 describe('Person service business rules', () => {
 	let peopleService: PeopleService;
@@ -10,86 +11,68 @@ describe('Person service business rules', () => {
 		);
 	});
 
+	const getPersonObject = <Type extends keyof Person>(key?: Type, value?: Person[Type]): Person => {
+		let object: Person = {
+			apelido: 'Willzinn', 
+			nome: 'Willson',
+			nascimento: '1920/07/23', 
+			stack: ['Node.js', 'Php', 'Typescript', 'Laravel', 'Python'],   	
+		};
+	
+		if (!key) return object;
+
+		object[key] = value as Person[Type];
+		return object;
+	};
+
 	describe('POST - Create person', () => {
 		it('should throw error if nickname is null', () => {
-			const object = {
-				apelido: null as unknown as string, 
-				nome: 'Willson',
-			  nascimento: '23/07/1920', 
-				stack: [],   	
-			};
+
+			const object = getPersonObject('apelido', null as unknown as string);
 
 			expect(() => peopleService.createPerson(object))
 				.toThrow('apelido is null');
 		});
 
 		it('should throw error if name is null', () => {
-			const object = {
-				apelido: 'Will', 
-				nome: null as unknown as string,
-			  nascimento: '23/07/1920', 
-				stack: [],   	
-			};
+			const object = getPersonObject('nome', null as unknown as string);
+
+			console.log(object);
 
 			expect(() => peopleService.createPerson(object))
 				.toThrow('nome is null');
 		});
 
 		it('should throw error if nickname is not a string', () => {
-			const object = {
-				apelido: 123 as unknown as string, 
-				nome: '',
-			  nascimento: '23/07/1920', 
-				stack: [],   	
-			};
+			const object = getPersonObject('apelido', 123 as unknown as string);
 
 			expect(() => peopleService.createPerson(object))
 				.toThrow('apelido is not a string');
 		});
 
 		it('should throw error if name is not a string', () => {
-			const object = {
-				apelido: '', 
-				nome: 123 as unknown as string,
-			  nascimento: '23/07/1920', 
-				stack: [],   	
-			};
+			const object = getPersonObject('nome', 123 as unknown as string);
 
 			expect(() => peopleService.createPerson(object))
 				.toThrow('nome is not a string');
 		});
 			
 		it('should throw error if date is not a valid date', () => {
-			const object = {
-				apelido: '', 
-				nome: '',
-			  nascimento: '23/07/20', 
-				stack: [],   	
-			};
+			const object = getPersonObject('nascimento', '20/10/20');
 
 			expect(() => peopleService.createPerson(object))
 				.toThrow('nascimento is not a date');
 		});
 
 		it('should throw error if stack is not an array of string', () => {
-			const object = {
-				apelido: '', 
-				nome: '',
-			  nascimento: '1920/10/10', 
-				stack: [123, 123, 123] as unknown as string[],   	
-			};
+			const object = getPersonObject('stack', [123 as unknown as string,123 as unknown as string, 123 as unknown as string]);
 
 			expect(() => peopleService.createPerson(object))
 				.toThrow('stack is not a string');
 		});
 
 		it('should not throw an error if data looks good', () => {
-			const object = {
-				apelido: 'Willzonn', 
-				nome: 'William',
-			  nascimento: '1920/10/10', 
-				stack: ['Node.js', 'Php', 'Typescript', 'Laravel', 'Python'],   	
-			};
+			const object = getPersonObject();
 
 			expect(() => peopleService.createPerson(object)).not
 				.toThrow('stack is not a string');
