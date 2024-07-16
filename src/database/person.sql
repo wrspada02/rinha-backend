19803fa70e1b0388b65d317d761696c10b654621
William Spada <williamrspada02@gmail.com>
2024-07-16 19:04:20 -0300

    chore: adjust SQL starting database script

--- src/database/person.sql
+++ src/database/person.sql
@@ -1,7 +1,7 @@
-CREATE TABLE IF NOT EXISTS PERSON (
+CREATE TABLE IF NOT EXISTS "Person" (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     apelido VARCHAR NOT NULL,
     nome VARCHAR NOT NULL,
     nascimento VARCHAR NOT NULL,
-    stack TEXT[] NOT NULL
+    stack VARCHAR[] NOT NULL
 );
